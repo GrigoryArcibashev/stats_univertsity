fc0c33a810c6d42c579079ef65e137a0ee081e4f
arcibashev71 <arcibashev71@yandex.ru>
2023-07-17 23:04:49 +0500

    FINAL REF

--- TV.py
+++ TV.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 
 
 class DistributionAndDensity:
-    def __init__(self, SA, MSD):
-        self._SA = SA
-        self._MSD = MSD
+    def __init__(self, sample_average, mean_square_deviation):
+        self._SA = sample_average
+        self._MSD = mean_square_deviation
 
     def draw_distribution_and_density(self):
         # the three sigma rule
@@ -33,16 +33,16 @@ class DistributionAndDensity:
             distribution_vals,
             density_vals,
             x_vals):
-        fig1, ax1 = plt.subplots(1, 1)
+        fig, ax = plt.subplots(1, 1)
         plt.xlabel("x")
         plt.ylabel("y")
-        ax1.plot(x_vals, distribution_vals, label="Распределение вероятностей")
-        ax1.plot(x_vals, density_vals, label="Плотность вероятности")
-        self._draw_mark_of_SA(ax1)
-        ax1.legend(loc="best", frameon=False)
+        ax.plot(x_vals, distribution_vals, label="Распределение вероятностей")
+        ax.plot(x_vals, density_vals, label="Плотность вероятности")
+        self._draw_mark_of_SA(ax)
+        ax.legend(loc="best", frameon=False)
 
-    def _draw_mark_of_SA(self, ax1):
-        ax1.plot(
+    def _draw_mark_of_SA(self, ax):
+        ax.plot(
                 [self._SA, self._SA],
                 [0, 0.95],
                 color="black",
@@ -51,31 +51,30 @@ class DistributionAndDensity:
 
 
 class Frequencies:
-    def __init__(self):
-        self._x_vals = [x for x in range(86, 100, 2)]
-        self._empirical_frs = [1, 6, 16, 29, 21, 10, 10]
-        self._theoretical_frs = \
-            [1.767, 6.231, 15.717, 24.552, 22.692, 15.345, 6.696]
+    def __init__(self, x_vals, empirical_frs, theoretical_frs):
+        self._x_vals = x_vals
+        self._empirical_frs = empirical_frs
+        self._theoretical_frs = theoretical_frs
 
     def draw_empirical_and_theoretical_frequencies(self):
-        fig2, ax2 = plt.subplots(1, 1)
+        fig, ax = plt.subplots(1, 1)
         plt.xlabel("x")
         plt.ylabel("n")
-        ax2.plot(
+        ax.plot(
                 self._x_vals,
                 self._empirical_frs,
                 label="Эмпирические частоты")
-        ax2.plot(
+        ax.plot(
                 self._x_vals,
                 self._theoretical_frs,
                 label="Теоретические частоты")
-        self._draw_vertical_lines(ax2)
-        ax2.legend(loc="best", frameon=False)
+        self._draw_vertical_lines(ax)
+        ax.legend(loc="best", frameon=False)
 
-    def _draw_vertical_lines(self, ax2):
+    def _draw_vertical_lines(self, ax):
         for x in self._x_vals:
             y_st = 0 if x < 90 else 5 if x < 96 else 2
-            ax2.plot(
+            ax.plot(
                     [x, x],
                     [y_st, 30],
                     color="black",
@@ -84,10 +83,12 @@ class Frequencies:
 
 
 def main():
-    SA = 92.86  # sample_average
-    MSD = 2.83  # mean square deviation
-    DistributionAndDensity(SA, MSD).draw_distribution_and_density()
-    Frequencies().draw_empirical_and_theoretical_frequencies()
+    DistributionAndDensity(92.86, 2.83).draw_distribution_and_density()
+    Frequencies(
+            [x for x in range(86, 100, 2)],  # x_k values
+            [1, 6, 16, 29, 21, 10, 10],
+            [1.767, 6.231, 15.717, 24.552, 22.692, 15.345, 6.696]) \
+        .draw_empirical_and_theoretical_frequencies()
     plt.show()
 
 
